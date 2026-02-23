c763008490effce7469cad156eeca535422073eb
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2026-02-23 14:35:30 +0100

    some lion case study cleanup

--- case_studies/lions.R
+++ case_studies/lions.R
@@ -1,4 +1,4 @@
-# installing dev version of packages
+### installing dev version of packages
 # devtools::install_github("janolefi/LaMa")
 # devtools::install_github("kaskr/RTMB", subdir = "RTMB")
 # devtools::install_github("janolefi/RTMBdist")
@@ -7,17 +7,27 @@ library(LaMa)
 library(RTMBdist)
 library(fmesher)
 library(viridis)
+library(leaflet)
 
-color <- c("orange", "deepskyblue")
+color <- c("orange", "deepskyblue", "seagreen2")
 
+# lood lion GPS data
 data <- readRDS("./data/lions.rds")
-
 head(data)
 nrow(data)
 
-hist(data$step, prob = TRUE)
-hist(data$angle, prob = TRUE)
 
+## plot data on satellite image
+leaflet() %>%
+  addProviderTiles(providers$Esri.WorldImagery) %>%
+  fitBounds(lng1 = min(data$x, na.rm = TRUE),
+            lat1 = min(data$y, na.rm = TRUE),
+            lng2 = max(data$x, na.rm = TRUE),
+            lat2 = max(data$y, na.rm = TRUE)) %>%
+  addCircleMarkers(lng = data$x,
+                   lat = data$y,
+                   radius = 0.5,
+                   color = "#00000040")
 
 
 # Homogeneous model -------------------------------------------------------
@@ -31,26 +41,26 @@ nll0 <- function(par) {
   mu <- exp(log_mu); REPORT(mu)
   sigma <- exp(log_sigma); REPORT(sigma)
   zprob <- plogis(logit_zprob); REPORT(zprob)
-  #kappa <- exp(log_kappa); REPORT(kappa); REPORT(mu_angle)
-  rho <- plogis(logit_rho); REPORT(rho)
+  rho <- plogis(logit_rho); REPORT(rho); REPORT(mu_a)
 
   allprobs <- matrix(1, length(step), N)
   ind <- which(!is.na(step) & !is.na(angle))
   for(j in 1:N) {
     allprobs[ind,j] <- dzigamma2(step[ind], mu[j], sigma[j], zprob[j]) *
-      dwrpcauchy(angle[ind], c(pi,0)[j], rho[j])
+      dwrpcauchy(angle[ind], mu_a[j], rho[j])
   }
   -forward(delta, Gamma, allprobs, trackID = ID)
 }
 
-## 2 state model
+## 2-state model
 
 par <- list(
   eta = rep(-2, 2),
-  log_mu = log(c(0.2, 2)),
-  log_sigma = log(c(0.2, 1.5)),
+  log_mu = log(c(0.01, 1)),
+  log_sigma = log(c(0.01, 1)),
   logit_zprob = qlogis(c(0.01, 0.01)),
-  logit_rho = qlogis(c(0.2, 0.3))
+  logit_rho = qlogis(c(0.2, 0.3)),
+  mu_a = c(pi, 0)
 )
 dat <- list(
   step = data$step,
@@ -59,7 +69,7 @@ dat <- list(
   N = 2
 )
 
-obj0 <- MakeADFun(nll0, par)
+obj0 <- MakeADFun(nll0, par, map = list(mu_a = factor(rep(NA, 2))))
 opt0 <- nlminb(obj0$par, obj0$fn, obj0$gr)
 
 mod0 <- report(obj0)
@@ -71,18 +81,18 @@ mod0$zprob
 mod0$Gamma
 delta <- mod0$delta
 
+par(mfrow = c(1,2))
 hist(data$step, prob = TRUE, bor = "white", breaks = 150, xlim = c(0,4), ylim = c(0,2))
 for(j in 1:2) {
   curve(delta[j] * dgamma2(x, mu[j], sigma[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
 }
 
-hist(data$angle, prob = TRUE, bor = "white", breaks = 50)
+hist(data$angle, prob = TRUE, bor = "white", breaks = seq(-pi, pi, length = 30))
 for(j in 1:2) {
   curve(delta[j] * dwrpcauchy(x, c(pi,0)[j], rho[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
 }
 curve(delta[1] * dwrpcauchy(x, c(pi,0)[1], rho[1]) +
         delta[2] * dwrpcauchy(x, c(pi,0)[2], rho[2]), add = TRUE, lwd = 2, lty = 2)
-
 
 pres_step <- pseudo_res(
   data$step,
@@ -97,21 +107,90 @@ plot(pres_step)
 
 mod0$states <- viterbi(mod = mod0)
 
-# idx <- 1:1000
+par(mfrow = c(1,1))
+idx <- 1:10000
 # plot(data$step[idx], col = color[mod0$states[idx]], type = "h")
-# plot(data$x[idx], data$y[idx], col = color[mod0$states[idx]], asp = 1, pch = 16)
+plot(data$x[idx], data$y[idx], asp = 1,type = "l")
+points(data$x[idx], data$y[idx], cex = 0.8,
+       col = scales::alpha(color[mod0$states[idx]], 0.4), pch = 20)
 
 
+## 3-state model
+
+par <- list(
+  eta = rep(-2, 6),
+  log_mu = log(c(0.01, 0.5, 1.5)),
+  log_sigma = log(c(0.01, 0.5, 1.5)),
+  logit_zprob = qlogis(rep(1e-3, 3)),
+  logit_rho = qlogis(c(0.2, 0.3, 0.4)),
+  mu_a = c(pi, pi, 0)
+)
+dat <- list(
+  step = data$step,
+  angle = data$angle,
+  ID = data$ID,
+  N = 3
+)
+
+obj0.3 <- MakeADFun(nll0, par)
+opt0.3 <- nlminb(obj0.3$par, obj0.3$fn, obj0.3$gr)
+
+mod0.3 <- report(obj0.3)
+
+mu <- mod0.3$mu
+sigma <- mod0.3$sigma
+rho <- mod0.3$rho
+delta <- mod0.3$delta
+
+
+par(mfrow = c(1,2))
+hist(data$step, prob = TRUE, bor = "white", breaks = 150, xlim = c(0,4), ylim = c(0,2))
+for(j in 1:3) {
+  curve(delta[j] * dgamma2(x, mu[j], sigma[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
+}
+
+hist(data$angle, prob = TRUE, bor = "white", breaks = seq(-pi, pi, length = 30))
+for(j in 1:3) {
+  curve(delta[j] * dwrpcauchy(x, c(pi,pi,0)[j], rho[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
+}
+curve(delta[1] * dwrpcauchy(x, pi, rho[1]) +
+        delta[2] * dwrpcauchy(x, pi, rho[2]) +
+        delta[3] * dwrpcauchy(x, 0, rho[3]), add = TRUE, lwd = 2, lty = 2)
+
+pres_step <- pseudo_res(
+  data$step,
+  "gamma2",
+  list(mean = mu, sd = sigma),
+  mod = mod0.3
+)
+plot(pres_step)
+
+mod0.3$states <- viterbi(mod = mod0.3)
+
+idx <- 1:10000
+# plot(data$step[idx], col = color[mod0$states[idx]], type = "h")
+plot(data$x[idx], data$y[idx], asp = 1,type = "l")
+points(data$x[idx], data$y[idx], cex = 0.8,
+       col = scales::alpha(color[mod0.3$states[idx]], 0.4), pch = 20)
+
+
+# We are setteling for 2 states:
+### Much overlap between states 2 and 3 (splitting movement into 2)
+### but TPM: basically no persistence -> discrimination between 2 and 3 seems random
+### Conclusion: probably we cannot infer different movement behaviours at this coarse temporal resolution.
+
+rm(obj0.3)
+gc()
 
 # Model with periodic variation in the tpm --------------------------------
 
-Z <- cosinor(1:24, period = c(24, 12))
+# We know lions are nocturnal so we want to capture this.
 
 nll1 <- function(par) {
   getAll(par, dat)
 
   Gamma <- tpm_g(Z, beta)
-  Gamma <- Gamma[,, tod]
+  Gamma <- Gamma[,, hour]
   Delta <- matrix(0, length(startInd), 2)
   for(i in 1:length(startInd)){
     Delta[i, ] <- stationary_p(Gamma[,,startInd[i]+0:23], t = 1)
@@ -133,6 +212,8 @@ nll1 <- function(par) {
 
 ## 2 state model
 
+Z <- cosinor(1:24, period = c(24, 12, 6)) # 3 sin/cos minimises AIC and BIC
+
 par <- list(
   beta = cbind(rep(-2, 2), matrix(0, 2, ncol(Z))),
   log_mu = log(c(0.01, 1)),
@@ -146,7 +227,7 @@ dat <- list(
   ID = data$ID,
   N = 2,
   Z = Z,
-  tod = data$tod,
+  hour = data$hour,
   startInd = calc_trackInd(data$ID)
 )
 
@@ -161,17 +242,20 @@ mod1 <- report(obj1)
 Gamma <- tpm_g(Z, mod1$beta)
 Delta <- stationary_p(Gamma)
 
+par(mfrow = c(1,1))
 plot(Delta[,2], type = "b", ylim = c(0,1), col = color[2], lwd = 2)
 
 mod1$states <- viterbi_g(mod = mod1)
 delta <- prop.table(table(mod1$states))
 
+
+par(mfrow = c(1,2))
 hist(data$step, prob = TRUE, bor = "white", breaks = 150, xlim = c(0,4), ylim = c(0,2))
 for(j in 1:2) {
   curve(delta[j] * dgamma2(x, mu[j], sigma[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
 }
 
-hist(data$angle, prob = TRUE, bor = "white", breaks = 50)
+hist(data$angle, prob = TRUE, bor = "white", breaks = seq(-pi, pi, length = 30))
 for(j in 1:2) {
   curve(delta[j] * dwrpcauchy(x, c(pi,0)[j], rho[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
 }
@@ -190,11 +274,11 @@ plot(pres_step)
 
 # Model with spatial field in the tpm -------------------------------------
 
-## Spatial part part of model
-loc <- cbind(data$x_int, data$y_int)  # Spatial coordinates
+# constructing the mesh for GMRF approximation to spatial field
+loc <- cbind(data$x_int, data$y_int) # coords with interpolated NAs
 
-bnd_inner <- fm_nonconvex_hull(loc, convex=0.04)
-bnd_outer <- fm_nonconvex_hull(loc, convex=0.15)
+bnd_inner <- fm_nonconvex_hull(loc, convex=0.04) # inner boundary
+bnd_outer <- fm_nonconvex_hull(loc, convex=0.15) # outer boundary
 
 mesh <- fm_mesh_2d(
   loc=loc,
@@ -205,12 +289,16 @@ mesh <- fm_mesh_2d(
   plot.delay=0.5
 )
 
+par(mfrow = c(1,1))
 plot(mesh)
+# points(data$x, data$y)
 
-spde <- fm_fem(mesh) # Calculate the sparse matrices c0,g1, g2 need for precission matrix
+# Calculate finite element matrices c0,g1, g2 need for precision matrix Q
+spde <- fm_fem(mesh)
 dim(spde$c0)
 
-X_p <- fm_basis(mesh, loc) # prediction matrix for observed locations
+# prediction matrix for observed locations
+X_p <- fm_basis(mesh, loc)
 
 jnll <- function(par) {
   getAll(par, dat, warn = FALSE)
@@ -220,8 +308,8 @@ jnll <- function(par) {
   zprob <- plogis(logit_zprob); REPORT(zprob)
   rho_angle <- plogis(logit_rho_angle); REPORT(rho_angle)
 
-  Eta <- matrix(eta, length(step), 2, byrow = TRUE)
-  Eta[,1] <- Eta[,1] + as.numeric(X_p %*% w) REPORT(w) # field only for Pr(active -> resting)
+  Eta <- matrix(eta, length(step), 2, byrow = TRUE); REPORT(eta)
+  Eta[,1] <- Eta[,1] + as.numeric(X_p %*% w); REPORT(w) # field only for Pr(active -> resting)
 
   Gamma <- tpm_g(Eta = Eta)
   Delta <- stationary(Gamma[,,startInd])
@@ -265,10 +353,10 @@ dat <- list(
   ID = data$ID,
   startInd = calc_trackInd(data$ID),
   X_p = X_p,
-  c0 = spde$c0,
-  g1 = spde$g1,
-  g2 = spde$g2,
-  bw = 8
+  c0 = spde$c0, # C
+  g1 = spde$g1, # G
+  g2 = spde$g2, # G C^{-1} G
+  bw = 10
 )
 
 t1 <- Sys.time()
@@ -285,12 +373,13 @@ mu <- mod_sp1$mu
 sigma <- mod_sp1$sigma
 rho_angle <- mod_sp1$rho_angle
 
+par(mfrow = c(1,2))
 hist(data$step, prob = TRUE, bor = "white", breaks = 150, xlim = c(0,4), ylim = c(0,2))
 for(j in 1:2) {
   curve(delta[j] * dgamma2(x, mu[j], sigma[j]), add = TRUE, col = color[j], lwd = 2, n = 1000)
 }
 
-hist(data$angle, prob = TRUE, bor = "white", breaks = 50)
+hist(data$angle, prob = TRUE, bor = "white", breaks = seq(-pi, pi, length = 30))
 for(j in 1:2) {
   curve(delta[j] * dwrpcauchy(x, c(pi,0)[j], rho_angle[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
 }
@@ -298,24 +387,24 @@ curve(delta[1] * dwrpcauchy(x, pi, rho_angle[1]) +
         delta[2] * dwrpcauchy(x, 0, rho_angle[2]), add = TRUE, lwd = 2, lty = 2)
 
 
-## estimated fields
-
-w <- obj_sp1$env$last.par.best[names(obj_sp1$env$last.par.best) == "w"]
+## visualise estimated field
 
 x_seq <- seq(min(data$x_int), max(data$x_int), length.out = 1024)
 y_seq <- seq(min(data$y_int), max(data$y_int), length.out = 1024)
 grid <- as.matrix(expand.grid(x_seq, y_seq))
 
-A <- fm_basis(mesh, grid)       # projection matrix (grid × vertices)
-field <- as.numeric(A %*% w)
+# Projection matrix on grid
+A <- fm_basis(mesh, grid)
+field <- as.numeric(A %*% mod_sp1$w) + mod_sp1$eta[1]
 
-z21 <- matrix(field, nrow = length(x_seq), ncol = length(y_seq))
+z1 <- matrix(field, nrow = length(x_seq), ncol = length(y_seq))
+g1 <- plogis(z1) # transition probability
 
 par(mfrow = c(1,1))
-image(x_seq, y_seq, z21,
+image(x_seq, y_seq, g1,
       xlab = "x", ylab = "y",
-      col = viridis(100),
-      main = "logit(Pr(active -> resting))", bty = "n", asp = 1)
+      col = viridis(35),
+      main = "Pr(active -> resting)", bty = "n", asp = 1)
 
 # idx <- which(mod_sp1$states == 1)
 # points(data$x[-idx], data$y[-idx], col = "#00000040")
@@ -335,7 +424,7 @@ jnll2 <- function(par) {
   rho_angle <- plogis(logit_rho_angle); REPORT(rho_angle)
 
   Eta <- cbind(1, Z) %*% t(beta); REPORT(beta) # periodic variation part, calculations for 1:24 only
-  Eta <- Eta[tod, ] # rep according to time of day
+  Eta <- Eta[hour, ] # rep according to time of day
   Eta[,1] <- Eta[,1] + as.numeric(X_p %*% w); REPORT(w) # field only for Pr(active -> resting)
   Gamma <- tpm_g(Eta = Eta)
   Delta <- stationary(Gamma[,,startInd])
@@ -361,14 +450,8 @@ jnll2 <- function(par) {
   nll
 }
 
-# initial field parameters
-# sigma0 <- 0.1 # marginal sd, initialise small
-# range <- diff(range(data$x, na.rm = TRUE)) * sqrt(2) / 5 # largest distance devided by 5 -> smooth field
-# kappa0 <- sqrt(8) / range
-# tau0 <- 1 / (sigma0 * sqrt(4*pi) * kappa0)
-
 par <- list(
-  beta = cbind(rep(-2, 2), matrix(0, 2, ncol(Z))),
+  beta = mod1$beta,
   log_mu = log(c(0.01, 1)),
   log_sigma = log(c(0.01, 1)),
   logit_zprob = qlogis(rep(1e-4, 2)),
@@ -385,12 +468,12 @@ dat <- list(
   ID = data$ID,
   startInd = calc_trackInd(data$ID),
   Z = Z,
-  tod = data$tod,
+  hour = data$hour,
   X_p = X_p,
   c0 = spde$c0,
   g1 = spde$g1,
   g2 = spde$g2,
-  bw = 8
+  bw = 10
 )
 
 t1 <- Sys.time()
@@ -407,44 +490,113 @@ mu <- mod_sp2$mu
 sigma <- mod_sp2$sigma
 rho_angle <- mod_sp2$rho_angle
 
-hist(data$step, prob = TRUE, bor = "white", breaks = 150, xlim = c(0,4), ylim = c(0,2))
+
+# pdf("./figs/lions_statedep.pdf", width = 7, height = 3.5)
+par(mfrow = c(1,2), mar = c(5,4,4,2)+0.1, xpd = FALSE)
+
+hist(data$step, prob = TRUE, bor = "white", breaks = 100,
+     main = "", xlab = "Step length (km)",
+     xlim = c(0,4), ylim = c(0,1))
 for(j in 1:2) {
   curve(delta[j] * dgamma2(x, mu[j], sigma[j]), add = TRUE, col = color[j], lwd = 2, n = 1000)
 }
+curve(delta[1] * dgamma2(x, mu[1], sigma[1])+
+        delta[2] * dgamma2(x, mu[2], sigma[2]), add = TRUE, lwd = 2, lty = 2, n = 1000)
 
-hist(data$angle, prob = TRUE, bor = "white", breaks = 50)
+hist(data$angle, prob = TRUE, bor = "white",
+     breaks = seq(-pi, pi, length = 30),
+     main = "", xlab = "Turning angle (radians)",
+     xaxt = "n")
+axis(1, at = seq(-pi, pi, by = pi/2),
+     labels = c(expression(-pi), expression(-pi/2), expression(0), expression(pi/2), expression(pi)))
 for(j in 1:2) {
   curve(delta[j] * dwrpcauchy(x, c(pi,0)[j], rho_angle[j]), add = TRUE, col = color[j], lwd = 2, n = 500)
 }
 curve(delta[1] * dwrpcauchy(x, pi, rho_angle[1]) +
-        delta[2] * dwrpcauchy(x, 0, rho_angle[2]), add = TRUE, lwd = 2, lty = 2)
+        delta[2] * dwrpcauchy(x, 0, rho_angle[2]), add = TRUE, lwd = 2, lty = 2, n = 500)
+
+# dev.off()
 
 
+## visualise estimated field
 
-## estimated fields
-
-w <- obj_sp2$env$last.par.best[names(obj_sp2$env$last.par.best) == "w"]
-
-x_seq <- seq(min(data$x_int), max(data$x_int), length.out = 1024)
-y_seq <- seq(min(data$y_int), max(data$y_int), length.out = 1024)
-grid <- as.matrix(expand.grid(x_seq, y_seq))
-
-A <- fm_basis(mesh, grid)       # projection matrix (grid × vertices)
-field <- as.numeric(A %*% w)
+field <- as.numeric(A %*% mod_sp2$w) + mod1$beta[1,1]
 
 z2 <- matrix(field, nrow = length(x_seq), ncol = length(y_seq))
+g2 <- plogis(z2)
 
-par(mfrow = c(1,1))
-image(x_seq, y_seq, z2,
-      xlab = "x", ylab = "y",
-      col = viridis(100),
-      main = "logit(Pr(active -> resting))", bty = "n", asp = 1)
+
+# cairo_pdf("./figs/lions_spatial_field.pdf", width = 7.365, height = 4.95)
+par(mfrow = c(1,1), mar = c(5,4,4,4), xpd = NA)
+image(x_seq, y_seq, g2,
+      xlab = "Longitude", ylab = "Latitude",
+      # col = viridis(300),
+      col = hcl.colors(35),
+      main = expression(Pr(active~"→"~resting)), bty = "n", asp = 1,
+      useRaster = TRUE)
+
+# Move legend outside plot (to the right)
+usr <- par("usr")
+legend_x <- c(usr[2] + 0.025 * diff(usr[1:2]),
+              usr[2] + 0.045 * diff(usr[1:2]))
+legend_y <- c(usr[3]+0.03,
+              usr[4]-0.03)
+legend_colors <- as.raster(matrix(rev(hcl.colors(100)), ncol = 1))
+
+rasterImage(legend_colors,
+            legend_x[1], legend_y[1],
+            legend_x[2], legend_y[2])
+
+legend_values <- round(seq(min(g2), max(g2), length.out = 6), 1)
+legend_positions <- seq(legend_y[1], legend_y[2], length.out = 6)
+
+text(legend_x[2] + 0.02 * diff(usr[1:2]),
+     legend_positions,
+     labels = legend_values,
+     adj = 0,
+     cex = 0.9)
+
+# dev.off()
+
+
+# # Define gradient legend position
+# legend_x <- c(22.7, 23.7)  # X range for legend
+# legend_y <- c(-21.08, -21.05)  # Adjust above the plot
+#
+# # Create a color gradient
+# legend_colors <- as.raster(matrix(hcl.colors(100), nrow = 1))
+#
+# # Draw the gradient legend
+# rasterImage(legend_colors, legend_x[1], legend_y[1], legend_x[2], legend_y[2])
+#
+# # Add numerical labels
+# legend_values <- round(seq(min(g2), max(g2), length.out = 6), 1)
+# legend_positions <- seq(legend_x[1], legend_x[2], length.out = 6)
+# text(legend_positions, legend_y[2]+0.02, labels = legend_values, cex = 0.9)
+
+
 
 # idx <- which(mod_sp2$states == 1)
-# points(data$x[-idx], data$y[-idx], col = "#00000040")
+# points(data$x, data$y,
+#        # col = "#00000015",
+#        col = scales::alpha("white", 0.1),
+#        pch = 16, cex = 0.2)
 
 
-Gamma <- tpm_g(Z, mod_sp$beta)
+# both fields
+par(mfrow = c(1,2))
+image(x_seq, y_seq, z1,
+      xlab = "x", ylab = "y",
+      col = viridis(15),
+      main = "spatial model", bty = "n", asp = 1)
+image(x_seq, y_seq, z2,
+      xlab = "x", ylab = "y",
+      col = viridis(15),
+      main = "spatial model with periodic var.", bty = "n", asp = 1)
+
+
+# periodically stationary distribution
+Gamma <- tpm_g(Z, mod_sp2$beta)
 Delta <- stationary_p(Gamma)
 
 plot(Delta[,2], type = "b", ylim = c(0,1), col = color[2], lwd = 2)
@@ -455,6 +607,7 @@ AIC(mod0, mod1, mod_sp1, mod_sp2)
 BIC(mod0, mod1, mod_sp1, mod_sp2)
 
 
+## residual comparison
 pres1 <- pseudo_res(
   data$step,
   "gamma2",
@@ -467,6 +620,9 @@ pres2 <- pseudo_res(
   list(mean = mu, sd = sigma),
   mod = mod_sp2
 )
+
 par(mfrow = c(2,3))
 plot(pres1)
 plot(pres2)
+
+
